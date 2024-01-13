52c8220a105cd72f84e76de06754d4efa09a5d9d
PrivateForrestGump <forrestleung58@gmail.com>
2024-01-13 14:53:37 +0800

    e

--- main.py
+++ main.py
@@ -4,34 +4,58 @@ Q1:
 '''
 
 # TODO: Write your code here
-
-##################################################
+# for i in range(1, 1000001, 1):
+#     print(i)
+# ##################################################
 '''
 Q2:
+
+result = 0
+for i in range(0, 1000001, 1):
+    result=result+i
+print(f'The sum is {result}')
 '''
 
 # TODO: Write your code here
-
 ##################################################
 '''
 Q3:
+
+for i in range(2, 100, 2):
+    print(i)
 '''
 
 # TODO: Write your code here
-
 ##################################################
 '''
 Q4:
+
+for i in range(97, 0, -3):
+    print(i)
 '''
 
 # TODO: Write your code here
-
 ##################################################
 '''
 Q5:
 '''
 
-# TODO: Write your code here
+# TODO: Write your code here 
+# a) 
+# for i in range(1, 11, 1):
+#     result = i*i
+#     print(result)
+
+#b)
+# result = 1
+# diff = 3
+
+# for i in range(0, 10, 1):
+#     print(result)
+#     result = result + diff
+#     diff = diff + 2
+
+
 
 ##################################################
 '''
@@ -39,5 +63,32 @@ Q6:
 '''
 
 # TODO: Write your code here
+# for i in range(1, 6, 1):
+#     print('*'*5)
+##################################################
+'''
+Q7:
+'''
 
+# TODO: Write your code here
+for i in range(1, 6, 1):
+     print('*' * i)
+##################################################
+'''
+Q8:
+'''
+
+# TODO: Write your code here
+for i in range(6, 1, -1):
+     print('*' * i)
+##################################################
+'''
+Q9:
+'''
+
+# TODO: Write your code here
+result = 1
+for i in range(1, 6, 1):
+     print(f'{result}' * i)
+     result = result + 1
 ##################################################
